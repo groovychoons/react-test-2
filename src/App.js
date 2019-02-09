fa2b99376714e4a0c6d9c47d1ff105deaf951f4a
Zara Siddique <zara@siddique.eu>
2019-02-09 13:29:45 +1100

    new project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <h1>Just a lil React test</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
+            <Button variant="contained" color="primary">
+      Hello World
+    </Button>
         </header>
       </div>
+
     );
   }
 }
